0f8e0048296fae1c9062c4294cd80c1e335d70eb
snk06 <43297746+snk06@users.noreply.github.com>
2019-10-12 15:05:00 -0400

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,40 @@
+# Level 1
+# Add up all the numbers in an array. Print out the total.
+
+# listOfNumbers = [1, 38, -28, 91, 193, 0]
+# sum = 0
+# for i in range (0, len(listOfNumbers)):
+#     print(listOfNumbers[i])
+#     sum+=(listOfNumbers)[i]
+#     print(sum)
+
+
+# Level 2
+# Given a list of students and a corresponding list of grades, print out the names of all students whose grade is 100.
+# In this example, the program should print
+# Ben
+# Carl
+
+# listOfStudents = ["Abby", "Ben", "Carl", "Dave"]
+# listOfGrades = [95, 100, 100, 92]
+# for i in range(0, len(listOfGrades)):
+#     if listOfGrades[i]==100:
+#         print(listOfStudents[i])
+
+
+
+
+# Level 3
+# Print out the sum of each row in a nested (2D) list
+# The output should be:
+# Sum of row 1: 10
+# Sum of row 2: 12
+# Sum of row 3: 13
+
+a = [ [ 2, 3, 5 ] ,
+      [ 1, 4, 7 ] ,
+      [ 3, 1, 9 ] ]
+row1=(a[0])[0]+(a[0])[1]+(a[0])[2]
+row2=
+print("Sum of row 1:", row1)
+
